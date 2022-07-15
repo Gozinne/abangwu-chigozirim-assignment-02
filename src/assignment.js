8e88e3b3fe57ef6d54b23f263c698ed1ce9f632c
Chigozirim Abangwu <80969889+Gozinne@users.noreply.github.com>
2022-07-15 21:19:42 +0000

    check challenge 1 & 2 done

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+  let sum = 0
+  for ( let i = 0; i < arrayOfNumbers.length; i++) 
+    { 
+      sum = sum + arrayOfNumbers[i]
+    }
+return sum
+} 
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+    let count = 0
+  for (let j = 0; j < arrayOfNumbers.length; j++) 
+    {
+      if (arrayOfNumbers[j] %2 == 0)
+      {
+        count++
+      }
+      }
+      return count
+    }
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
